2b195ec4e38fc87e946f0d6a41225a8562ffc776
Jennifer Delgado <jennifer.delgado27@gmail.com>
2014-04-03 22:54:44 -0500

    Changes to nav bar and home page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function responsivize () {
-  $ ('.featured-image')
+  $ ('.responsive')
   		.css({
   		  height: $(window).height() + 'px',
   		  width: $(window).width() + 'px',
@@ -11,4 +11,15 @@ $(document).ready(function() {
 	$('.featured-image').imgLiquid();
 	responsivize();
 	$(window).on('resize', responsivize);
+
+	$("div.featured-image").click(function() {
+		$("html, body").animate({
+			"scrollTop": $('#about').offset().top-100
+		});
+		$("nav").css({
+			display: "inline"
+		});
+		responsivize();
+	});
 });
+
